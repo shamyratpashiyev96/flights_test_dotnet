815fd11d9b294f6a5f58251f5a16f53f832c5113
shamurat <it.shamurat@st.com.tm>
2023-10-08 14:02:18 +0500

    Connected the app to Mysql

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace testApp.Data;
+
+public class AppDbContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //  #warning To protect potentially sensitive information in your connection string, 
+        //  you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 
+        //  for guidance on storing connection strings.
+        
+        optionsBuilder.UseMySQL("server=localhost;database=dummy_db;user=dummy_user;password=PASSword123$%");
+    }
+}
